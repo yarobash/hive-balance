6e2a86d2003258b159c9e26c60664f7b0b586a27
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-02-12 13:46:29 +0300

    improvement(daily-increase): add description

--- back/hive-server/notifications/daily-increase.sh
+++ back/hive-server/notifications/daily-increase.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#######################################################################
+# Get daily weight difference of each hive, compose message and
+# send via telegram bot.
+#######################################################################
 
 set -o nounset
 set -o errexit
