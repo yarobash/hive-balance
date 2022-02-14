402f4d1afcae787a7cd1e61cc4e8b37b4f21e674
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-02-14 23:48:19 +0300

    feat(notifications/system-status.sh): add check_arguments() and parse_arguments() draft

--- back/hive-server/notifications/system-status.sh
+++ back/hive-server/notifications/system-status.sh
@@ -4,6 +4,12 @@
 # Collect current system status adn send via telegram bot.
 ######################################################################
 
+check_arguments() {
+}
+
+parse_arguments() {
+}
+
 get_current_state() {
 }
 
