60dba2d1eb44133b20cb7000f845891e8588a8c8
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-02-22 22:47:28 +0300

    improvement(back/db): clarify apiary model

--- back/hive-server/models/user.ts
+++ back/hive-server/models/user.ts
@@ -23,10 +23,4 @@ const schema = new Schema<User>({
     type: String,
     required: true,
   },
-
-  apiaries: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'apiary',
-    default: [],
-  }],
 });
