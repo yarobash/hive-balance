4d77e845a638006cfd1bad9254851605a1b35663
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-03-01 22:04:16 +0300

    feat(app): add createUser route

--- back/hive-server/src/app.ts
+++ back/hive-server/src/app.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import createUser from './routes/signup.ts';
 
-const PORT=3000;
+const PORT = 3000;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/hivedb');
+
+app.use(express.json());
+app.use('/signup', createUser);
+
 app.listen(PORT);
