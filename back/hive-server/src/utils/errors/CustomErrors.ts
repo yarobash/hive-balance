1fc1a46e6f78efb819262cd0b946656fd865fb9d
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-03-04 21:33:28 +0300

    feat(errors): add custorm error 409

--- back/hive-server/src/utils/errors/CustomErrors.ts
+++ back/hive-server/src/utils/errors/CustomErrors.ts
@@ -1,4 +1,4 @@
-export class CustomError400 extends Error {
+export class Error400 extends Error {
   statusCode: number;
 
   constructor(message: string) {
@@ -7,11 +7,20 @@ export class CustomError400 extends Error {
   }
 }
 
-export class CustomError401 extends Error {
+export class Error401 extends Error {
   statusCode: number;
 
   constructor(message: string) {
     super(message);
     this.statusCode = 401;
+  }
+}
+
+export class Error409 extends Error {
+  statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+    this.statusCode = 409;
   }
 }
