d8c53ce97e90d701f77847dd69f112f84d8b4e9b
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-03-31 21:49:34 +0300

    feat(constants): add err messages of hives queries as constants

--- back/hive-server/src/models/statics/hive.ts
+++ back/hive-server/src/models/statics/hive.ts
@@ -1,6 +1,7 @@
 import { Types } from 'mongoose';
 import CustomError from '../../utils/errors/CustomError';
 import fullErrMsg from '../../utils/errors/fullErrMsg';
+import errMsgs from '../../shared/constants/errorMessages';
 
 export default {
   createHive(title: string, apiary: Types.ObjectId, owner: Types.ObjectId, frames: [string]) {
@@ -11,14 +12,14 @@ export default {
 
   getHiveById(id: Types.ObjectId) {
     return this.findById(id)
-      .orFail(new CustomError(404, `Hive with _id: ${id} not found`))
+      .orFail(new CustomError(404, errMsgs.nonexistentHive(id)))
       .then((hive: any) => hive)
       .catch((err: any) => Promise.reject(err));
   },
 
   getHivesByApiaryId(apiaryId: Types.ObjectId, ownerId: Types.ObjectId) {
     return this.find({apiary: apiaryId, owner: ownerId})
-      .orFail(new CustomError(404, `Apiary with id: ${apiaryId} or owner with id: ${ownerId} not found`))
+      .orFail(new CustomError(404, errMsgs.nonexistentHives(apiaryId, ownerId)))
       .then((hives: any) => hives)
       .catch((err: any) => Promise.reject(err));
   },
@@ -29,7 +30,7 @@ export default {
       {title: newTitle},
       {new: true},
     )
-    .orFail(new CustomError(404, `Hive with id: ${hiveId} not found`))
+    .orFail(new CustomError(404, errMsgs.nonexistentHive(hiveId)))
     .then((hive: any) => hive)
     .catch((err: any) => Promise.reject(err));
   },
@@ -40,7 +41,7 @@ export default {
       {frames: newFrames},
       {new: true},
     )
-    .orFail(new CustomError(404, `Hive with id: ${hiveId} not found`))
+    .orFail(new CustomError(404, errMsgs.nonexistentHive(hiveId)))
     .then((hive: any) => hive)
     .catch((err: any) => Promise.reject(err));
   },
--- back/hive-server/src/shared/constants/errorMessages.ts
+++ back/hive-server/src/shared/constants/errorMessages.ts
@@ -4,5 +4,7 @@ export default {
   nonexistentFrame: (id: any) => `Frame: ${id} doesn't exist`,
   nonexistentFrams: () => 'Frames don\'t exist',
   nonexistentStandardFrame: () => 'Frame with type: standard doesn\'t exist',
-  ownerlessFrame: (owner) => `Frame with owner: ${owner} doesn't exist`,
+  ownerlessFrame: (owner: any) => `Frame with owner: ${owner} doesn't exist`,
+  nonexistentHive: (id: any) => `Hive: ${id} doesn't exist`,
+  nonexistentHives: (apiaryId: any, ownerId: any) => `Apiary with id: ${apiaryId} or owner with id: ${ownerId} not found`,
 };
