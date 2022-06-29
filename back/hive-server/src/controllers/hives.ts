8bf4ef420b63322070d63f3442ec9ffabda54c04
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-06-29 17:21:10 +0300

    hot-fix(app): typos

--- back/hive-server/src/controllers/hives.ts
+++ back/hive-server/src/controllers/hives.ts
@@ -6,7 +6,6 @@ export default {
   createHive(req: any, res: any, next: any) {
     const owner = req.user._id;
     const { title, apiary, frames } = req.body;
-    console.log(title, apiary, frames); 
     hive.createHive(title, apiary, owner, frames)
       .then((hive: any) => res.send(hiveAnswer(hive)))
       .catch(next);
