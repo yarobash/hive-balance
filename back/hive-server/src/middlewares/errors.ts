a86bf4dc76d411eca0916787704108dd619d17cb
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-06-30 16:33:40 +0300

    refactor(app): del console.logs

--- back/hive-server/src/middlewares/errors.ts
+++ back/hive-server/src/middlewares/errors.ts
@@ -1,6 +1,4 @@
 export default (err: any, req: any, res: any, next: any) => {
-  console.log("Doshlo do suda");
-  console.log(err);
   const statusCode: any = err.statusCode || 500;
   const message: string = statusCode === 500
     ? 'Внутренняя ошибка сервера'
